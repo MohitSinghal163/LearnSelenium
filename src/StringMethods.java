732b2fa490bfa1dd7c5a921efe4fe599fe4ca62c
MohitSinghal163 <Mohitsinghal163@gmail.com>
2022-07-27 20:33:01 +0530

    Modified the code to include function

--- src/StringMethods.java
+++ src/StringMethods.java
@@ -3,9 +3,17 @@ public class StringMethods {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("1st program on 14 inch macbook");
+		String stringInput = "Mohit Singhal" , stringOutput;
+		stringOutput = reverse(stringInput);
+		
+		System.out.println(stringOutput);
 		System.out.println("Pushing the changes");
 
+	}
+	
+	public static String reverse(String inputString)
+	{
+		return inputString;
 	}
 
 }
