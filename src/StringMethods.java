e0935553d2d836f430e69e01aa432be5ef8e31a0
MohitSinghal163 <Mohitsinghal163@gmail.com>
2022-07-27 20:27:49 +0530

    2nd Commit with code

--- src/StringMethods.java
+++ src/StringMethods.java
@@ -4,6 +4,7 @@ public class StringMethods {
 	public static void main(String[] args) 
 	{
 		System.out.println("1st program on 14 inch macbook");
+		System.out.println("Pushing the changes");
 
 	}
 
